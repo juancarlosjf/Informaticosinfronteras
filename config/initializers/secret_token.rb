3121bc3ad32aa682759450724adc09775d4c2a83
Juan Carlos Jimenez Flores <carlos.jf.1681@gmail.com>
2012-09-19 22:59:15 -0500

    Juan Carlos
    Esqueleto de proyecto generado con ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appmobile::Application.config.secret_token = '0fcab824ee588c7a62347b868fdaa978f6027034944c305dc034eba1f87caaa0dd2809c09b3a6a38787fb03ef7f4b03e26dcf1eb7e090aea126d0067ba368c28'
